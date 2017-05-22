6522e8fb407250e042a6cd6ee7ac4e3a0e561d1a
Samuel Kuang <samuelkuang@outlook.com>
2017-05-22 12:42:42 -0700

    Webpack tutorial

---
+++ src/app.js
@@ -0,0 +1,6 @@
+const css = require('./app.scss');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render( < h1 > Hello, world!Again < /h1> , document.getElementById("root"));
