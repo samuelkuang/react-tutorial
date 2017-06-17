80948ebc395b42efa53f2a114decbe4c99c13421
Samuel Kuang <samuelkuang@outlook.com>
2017-06-17 15:19:04 -0700

    Move app.js to components folder

--- src/app.js
+++
@@ -1,7 +0,0 @@
-const css = require('./app.scss');
-
-import $ from "jquery";
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-ReactDOM.render( < h1 > Hello, world!Again < /h1> , document.getElementById("root"));
